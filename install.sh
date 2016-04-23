62f0bd57126f4e7802c7876d9d55d35e527f3f41
Lasse S. Haslev <lasse@haslev.no>
2016-04-23 19:48:59 +0200

    Added Vundle bundle folder

--- install.sh
+++ install.sh
@@ -4,4 +4,9 @@ echo '" Import vim files
 source ~/.vim/runtime/vimrcs/import.vim
 ' > ~/.vimrc
 
+mkdir ~/.vim/vimundo
+
+echo "Setting up Vundle"
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 echo "Installed the Ultimate Vim."
